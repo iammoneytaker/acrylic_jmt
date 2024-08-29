44034adb3e7ea5a1c370859b373946c6efdb1893
Sangwon <dnjs2618@naver.com>
2024-08-29 12:36:09 +0900

    FIX ERROR

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
-    unoptimized: true, // 이미지 최적화를 비활성화합니다
+    domains: ['www.acrylic-jmt.co.kr'],
+    path: '/_next/image',
+    loader: 'default',
   },
 };
 
