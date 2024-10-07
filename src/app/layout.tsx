3a0ea3a9a3cd8683c4af82d61703a12ba699bf4e
Sangwon <dnjs2618@naver.com>
2024-10-07 13:38:10 +0900

    ADD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,6 +103,15 @@ export default function RootLayout({
             }),
           }}
         />
+        <Script id="channelTalk" strategy="afterInteractive">
+          {`
+            (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+
+            ChannelIO('boot', {
+              "pluginKey": "db8528bb-892b-463e-95a5-56ad7b9fd2ae"
+            });
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <Header />
